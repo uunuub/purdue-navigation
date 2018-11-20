91e914377658daf0e331c4f88f669e2f77cdff9b
unumunkhb <agitouunuu@gmail.com>
2018-11-20 14:10:21 -0500

    Minimial Flask App Created

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import re
+from flask import Flask, jsonify, render_template, request, url_for
+from flask_jsglue import JSGlue
+
+# configure application
+app = Flask(__name__)
+JSGlue(app)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+@app.route("/")
+def index():
+    """Check for API_KEY"""
+    if not os.environ.get("API_KEY"):
+        raise RuntimeError("API_KEY not set")
+    #return render_template("index.html", key=os.environ.get("API_KEY"))
+    return "Hello"
