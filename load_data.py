f1bfacb6c873be24137166f6dd9ec222408596bc
uunuub <agitouunuu@gmail.com>
2018-11-22 06:47:19 -0500

    Updated requirements and load_data

--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from contextlib import closing
 from urllib.parse import urlencode
 
 import urllib3.contrib.pyopenssl 
@@ -26,6 +27,7 @@ def getSchedule():
     urllib3.contrib.pyopenssl.inject_into_urllib3()
 
     # Form query url with current day
+    params = {"days": today, "subject": "CS"}
     query_url = SCHEDULE_URL + "?" + "days=" + today + "&subject=CS"
 
     # PoolManager instance to make requests
@@ -36,6 +38,32 @@ def getSchedule():
     
     return req.data
 
-    
+def parseSchedule():
+    # Get scraped schedule page
+    raw_sched = getSchedule()
+    html = BeautifulSoup(raw_sched, "html5lib")
+
+    print(html.prettify())
+
+    # Find the text content within th tags and split them by "-" to get course titles
+    course_titles = []
+    for tag in html.findAll("th", {"class": "ddlabel"}):
+        current_course = [] 
+        for content in tag.a.contents[0].split("-"):
+            current_course.append(content.strip(" "))
+        course_titles.append(current_course)
+
+    for i in course_titles:
+        print(i)
+
+    # Find all course details
+    course_details = []
+    for tag in html.findAll("table"):
+        print(tag.string)
+        for content in tag.tbody:
+            print(content.findAll("td"))
+
+
+parseSchedule() 
 
 
