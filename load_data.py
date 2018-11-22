70a6099e874ef9ec8ca595d74fcc1a4138cb7d05
uunuub <agitouunuu@gmail.com>
2018-11-22 12:41:06 -0500

    Added ORM

--- load_data.py
+++ load_data.py
@@ -14,55 +14,63 @@ import datetime
 SCHEDULE_URL = "https://selfservice.mypurdue.purdue.edu/prod/bzwsrch.p_search_schedule"
 
 def getToday():
-    # Get current day numbeR
-    weekdays = ("M", "T", "W", "R", "F", "S", "U")
-    # Current date
-    return weekdays[datetime.datetime.today().weekday()]
+	# Get current day numbeR
+	weekdays = ("M", "T", "W", "R", "F", "S", "U")
+	# Current date
+	return weekdays[datetime.datetime.today().weekday()]
 
 def getSchedule():
-    # Get today one character form
-    today = getToday()
+	# Get today one character form
+	today = getToday()
 
-    # Enable certificate
-    urllib3.contrib.pyopenssl.inject_into_urllib3()
+	# Enable certificate
+	urllib3.contrib.pyopenssl.inject_into_urllib3()
 
-    # Form query url with current day
-    params = {"days": today, "subject": "CS"}
-    query_url = SCHEDULE_URL + "?" + "days=" + today + "&subject=CS"
+	# Form query url with current day
+	params = {"days": today, "subject": "CS"}
+	query_url = SCHEDULE_URL + "?" + "days=" + today + "&subject=CS"
 
-    # PoolManager instance to make requests
-    http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
+	# PoolManager instance to make requests
+	http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
 
-    # Request page containing schedule
-    req = http.request("GET", query_url)
-    
-    return req.data
+	# Request page containing schedule
+	req = http.request("GET", query_url)
+	
+	return req.data
 
 def parseSchedule():
-    # Get scraped schedule page
-    raw_sched = getSchedule()
-    html = BeautifulSoup(raw_sched, "html5lib")
+	# Get scraped schedule page
+	raw_sched = getSchedule()
+	html = BeautifulSoup(raw_sched, "html5lib")
 
-    print(html.prettify())
+	# Find the text content within th tags and split them by "-" to get course titles
+	course_titles = []
+	for tag in html.findAll("th", {"class": "ddlabel"}):
+		current_course = [] 
+		for content in tag.a.contents[0].split("-"):
+			current_course.append(content.strip(" "))
+		course_titles.append(current_course)
+	for i in course_titles:
+		print(i)
 
-    # Find the text content within th tags and split them by "-" to get course titles
-    course_titles = []
-    for tag in html.findAll("th", {"class": "ddlabel"}):
-        current_course = [] 
-        for content in tag.a.contents[0].split("-"):
-            current_course.append(content.strip(" "))
-        course_titles.append(current_course)
-
-    for i in course_titles:
-        print(i)
-
-    # Find all course details
-    course_details = []
-    for tag in html.findAll("table"):
-        print(tag.string)
-        for content in tag.tbody:
-            print(content.findAll("td"))
-
+	# Find all course details
+	course_details = []
+	for tag in html.findAll("table", {"class": "datadisplaytable"}):
+		# Skip if it's not a course tr tag
+		if(len(tag.contents) < 3):
+			continue
+		current_course = []
+		# Get all td tags within course tr tag
+		for td in tag.tbody.findNext("tr").findNext("tr").findAll("td"):
+			if len(td.contents) == 1:
+				current_course += td.contents	
+			# Avoid additionall tags within instructor tags	
+			else:
+				current_course.append(td.contents[0][:-2])
+		course_details.append(current_course)	
+	
+	# for i in course_details:
+	# 	print(i)
 
 parseSchedule() 
 
---
+++ models.py
@@ -0,0 +1,107 @@
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
+# Intatiate SQLAlchemy object
+db = SQLAlchemy()
+
+# Base model for other to inherit from
+class Base(db.Model):
+	__abstract__ = True
+
+	# Base attributes
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp(),
+							onupdate=db.func.current_timestamp())
+
+# Model for course time
+class Time(Base):
+	__tablename__ = "time" 
+	start_time = db.Column(db.Time(timezone=True))
+	end_time = db.Column(db.Time(timezone=True))
+
+	def __repr__(self):
+		return (self.start_time.strftime("%H:%M") + " - " + self.end_time.strftime("%H:%M"))
+
+# Model for instructor
+class Instructor(Base):
+	__tablename__ = "instructor" 
+	instructor = db.Column(db.String(50), unique=True)
+
+	def __repr__(self):
+		return self.instructor
+
+# Model for course type
+class Type(Base):
+	__tablename__ = "type" 
+	stype = db.Column(db.String(50), unique=True)
+	
+	def __repr__(self):
+		return self.stype
+ 
+# Model for room
+class Room(Base):
+	__tablename__ = "room" 
+	room = db.Column(db.String(50))
+	
+	def __repr__(self):
+		return self.room
+ 
+# Model for building
+class Building(Base):
+	__tablename__ = "building" 
+	building = db.Column(db.String(50), unique=True)
+	
+	def __repr__(self):
+		return self.building
+
+# Model for crn
+class CRN(Base):
+	__tablename__ = "crn" 
+	crn = db.Column(db.Integer, unique=True)
+	
+	def __repr__(self):
+		return str(self.crn)
+
+# Model for course number
+class Number(Base):
+	__tablename__ = "number" 
+	number = db.Column(db.Integer, unique=True)
+
+	def __repr__(self):
+		return str(self.number)
+
+# Model for course name
+class Name(Base):
+	__tablename__ = "name" 
+	name = db.Column(db.String(50), unique=True)
+
+	def __repr__(self):
+		return self.name
+
+# Model for course
+class Course(Base):
+	__tablename__ = "course" 
+	# Foreign Keys of other tables
+	name_id = db.Column(db.Integer, db.ForeignKey("name.id"))
+	number_id = db.Column(db.Integer, db.ForeignKey("number.id"))
+	crn_id = db.Column(db.Integer, db.ForeignKey("crn.id"))
+	building_id = db.Column(db.Integer, db.ForeignKey("building.id"))
+	room_id = db.Column(db.Integer, db.ForeignKey("room.id"))
+	type_id = db.Column(db.Integer, db.ForeignKey("type.id"))
+	instructor_id = db.Column(db.Integer, db.ForeignKey("instructor.id"))
+	time_id = db.Column(db.Integer, db.ForeignKey("time.id"))
+	# Bitwise operations will be used to extract available days
+	days = db.Column(db.CHAR(1), default=0)
+
+	# Connect other models to Course
+	name = db.relationship("Name", foreign_keys=[name_id], backref=db.backref("nameID", lazy="dynamic"))
+	number = db.relationship("Number", foreign_keys=[number_id], backref=db.backref("numberID", lazy="dynamic"))
+	crn = db.relationship("CRN", foreign_keys=[crn_id], backref=db.backref("crnID", lazy="dynamic"))
+	building = db.relationship("Building", foreign_keys=[building_id], backref=db.backref("buildingID", lazy="dynamic"))
+	room = db.relationship("Room", foreign_keys=[room_id], backref=db.backref("roomID", lazy="dynamic"))
+	stype = db.relationship("Type", foreign_keys=[type_id], backref=db.backref("stypeID", lazy="dynamic"))
+	instructor = db.relationship("Instructor", foreign_keys=[instructor_id], backref=db.backref("instructorID", lazy="dynamic"))
+	time = db.relationship("Time", foreign_keys=[time_id], backref=db.backref("time", lazy="dynamic"))
+
+
