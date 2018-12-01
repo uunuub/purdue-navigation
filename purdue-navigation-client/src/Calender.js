354f78befcf185fc11241c4d5e0f66a656ba14b6
Dhruv Ramanujan <dramanu@purdue.edu>
2018-12-01 17:24:02 -0500

    Reducing calender component to essentials

--- purdue-navigation-client/src/Calender.js
+++ purdue-navigation-client/src/Calender.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Calender.css';
 
 class Calender extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="Calender">
+        <header className="Calender-header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Add code magic here
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+
+
       </div>
     );
   }
--- purdue-navigation-client/src/index.js
+++ purdue-navigation-client/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Calender from './Calender';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Calender />, document.getElementById('root'));
 
 
