948c42435f7c14c72a97857ecb3278f4530b54c0
Dhruv Ramanujan <dramanu@purdue.edu>
2018-12-02 12:07:37 -0500

    Added functionality for RoomList to accept parameters

--- purdue-navigation-client/src/App.js
+++ purdue-navigation-client/src/App.js
@@ -4,10 +4,31 @@ import RoomList from './RoomList';
 import './App.css';
 
 class App extends Component {
+
+  constructor (){
+    super()
+
+    let testRooms = [
+      {name: "Room 1", free: 1, change: "1:30"},
+      {name: "Room 2", free: 1, change: "2:30"},
+      {name: "Room 3", free: 0, change: "5:30"},
+      {name: "Room 4", free: 0, change: "8:30"},
+      {name: "Room 5", free: 0, change: "12:30"},
+      {name: "Room 6", free: 1, change: "4:30"},
+      {name: "Room 7", free: 1, change: "3:30"},
+    ]
+
+    this.state = {
+      testRooms: testRooms
+    }    
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <RoomList />
+        <RoomList rooms={this.state.testRooms} propTest="pleaseworkman" />
       </div>
     );
   }
--- purdue-navigation-client/src/RoomList.js
+++ purdue-navigation-client/src/RoomList.js
@@ -3,23 +3,15 @@ import './RoomList.css';
 
 class RoomList extends Component {
 
-  constructor (){
-    super()
+  constructor (props){
+    super(props)
+    // this.constructor.bind(this)
 
-    let testRooms = [
-      {name: "Room 1", free: 0, change: "1:30"},
-      {name: "Room 2", free: 0, change: "2:30"},
-      {name: "Room 3", free: 1, change: "5:30"},
-      {name: "Room 4", free: 1, change: "8:30"},
-      {name: "Room 5", free: 1, change: "12:30"},
-      {name: "Room 6", free: 0, change: "4:30"},
-      {name: "Room 7", free: 0, change: "3:30"},
-    ]
-
+    let testRooms = this.props.rooms
     let roomArray = []
 
     for (let i = 0; i < testRooms.length; i++){
-      if (testRooms[i].free === 0){
+      if (testRooms[i].free === 1){
         roomArray.push(React.createElement("li", {className: "Room-box-free"}, (testRooms[i].name + " - Free until " + testRooms[i].change)))
       }
       else{
@@ -35,17 +27,15 @@ class RoomList extends Component {
   render() {
     return (
       <div className="RoomList">  
-      
         <header>
           <p>
             Building
+            {this.props.propTest}
           </p>
         </header>
-
         <ul>
           {this.state.roomList}
         </ul>
-
       </div>
     );
   }
