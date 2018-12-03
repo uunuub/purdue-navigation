0aa1bb0c674e6ed54a6105d353ab305f8abc3118
unumunkhb <agitouunuu@gmail.com>
2018-12-02 20:24:12 -0500

    merge lul.

---
+++ purdue-navigation-client/src/components/RoomList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './RoomList.css';
+
+class RoomList extends Component {
+
+  constructor (props){
+    //Accept list of rooms from App.js
+    super(props)
+    let rooms = this.props.rooms
+    let roomArray = []
+
+    //Converts the list of rooms to HTML Elements
+    for (let i = 0; i < rooms.length; i++){
+      if (rooms[i].free === 1){
+        roomArray.push(React.createElement("li", {className: "Room-box-free"}, (rooms[i].name + " - Free until " + rooms[i].change)))
+      }
+      else{
+        roomArray.push(React.createElement("li", {className: "Room-box-used"}, (rooms[i].name + " - Used until " + rooms[i].change)))
+      }
+    }
+
+    this.state = {
+      roomArray
+    }    
+  }
+
+
+  render() {
+    return (
+      <div className="RoomList">  
+        <header>
+          <p>
+            Building
+          </p>
+        </header>
+        <ul>
+          {this.state.roomArray}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default RoomList;
