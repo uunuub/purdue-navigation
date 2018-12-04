fc307ac6161a1d57137ad5d48a33ee88b49e3de0
unumunkhb <agitouunuu@gmail.com>
2018-12-04 03:19:16 -0500

    proper file control

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
-cd purdue-navigation-client && npm run build
+# Make sure npm has the packages
+cd purdue-navigation-client && npm install
 
+# Generate production code
+npm run build
+
+# Move build to above for flask to use
 mv build ../ && cd ..
 
+# Deploy 
 gcloud app deploy
